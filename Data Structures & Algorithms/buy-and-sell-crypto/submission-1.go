05f5f672c038eed1c454d2d6f5a6b784b44e4bd1
Aayush Rangwala <ayush.rangwala@gmail.com>
2026-08-04 14:02:49 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.go
@@ -0,0 +1,33 @@
+func maxProfit(prices []int) int {
+	if len(prices) < 2 {
+		return 0
+	}
+
+	small := 0
+	large := 1
+	profit := 0
+
+	for small < len(prices) && large < len(prices) {
+		if prices[large] > prices[small] {
+			profit = max(profit, prices[large] - prices[small])
+			large++
+			continue
+		}
+		// profit = max(profit, prices[large-1] - prices[small])
+
+		small = large
+		large++
+	}
+
+	// profit = max(profit, prices[large-1] - prices[small])
+
+	return profit
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
